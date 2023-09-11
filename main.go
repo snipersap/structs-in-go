079030f77e8979727acb08b84f13b8d233d03790
snipersap <snipersap@gmail.com>
2023-09-11 22:10:11 +0200

    update struct through pointer

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ type contactInfo struct {
 	pincode int
 }
 
+// Entry to structs-in-go
 func main() {
 	// Create person values
 	alex := person{"Alex", "Hardy", contactInfo{"h@h.com", 99888}} // Order of the field is important, all fields must
@@ -33,12 +34,34 @@ func main() {
 	priti.contact = contactInfo{email: "priti@sharma.com", pincode: 23099}
 	priti.printf()
 
+	//Try to update first name of sandy
+	sandy.updateFirstName(("Cindy"))
+	sandy.print() //First name did not update - no error too
+
+	// Try to update Name with pointers
+	sandyPointer := &sandy
+	sandyPointer.updateName("Cindy", "Klara")
+	sandy.print()
+
 }
 
+// Update the name of the person through a pointer
+func (pointerToPerson *person) updateName(newFirstName string, newLastName string) {
+	(*pointerToPerson).firstName = newFirstName
+	(*pointerToPerson).lastName = newLastName
+}
+
+// Tries to update the first name of the struct person
+func (p person) updateFirstName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+// Prints the person struct directly
 func (p person) print() {
 	fmt.Println(p)
 }
 
+// Prints the person struct in a given format
 func (p person) printf() {
 	fmt.Printf("%+v \n", p)
 }
