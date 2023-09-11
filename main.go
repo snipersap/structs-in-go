a43054badac563f501da95a0fdc92cc9e8372fee
snipersap <snipersap@gmail.com>
2023-09-11 21:11:16 +0200

    struct with default and assigned values

--- main.go
+++ main.go
@@ -10,14 +10,27 @@ type person struct {
 
 func main() {
 	// Create person values
-	alex := person{"Alex", "Hardy"}
+	alex := person{"Alex", "Hardy"} // Order of the field is important
 	sandy := person{firstName: "Sandy", lastName: "Homie"}
 
 	// Print person values to the screen
-	alex.print()
-	sandy.print()
+	alex.print()  //{Alex Hardy}
+	sandy.print() //{Sandy Homie}
+
+	// Create person with default values
+	var priti person
+	priti.print()  //{ }
+	priti.printf() //{firstName: lastName:}
+	priti.firstName = "Priti"
+	priti.lastName = "Sharma"
+	priti.printf() //{firstName:Priti lastName:Sharma}
+
 }
 
 func (p person) print() {
 	fmt.Println(p)
+}
+
+func (p person) printf() {
+	fmt.Printf("%+v \n", p)
 }
