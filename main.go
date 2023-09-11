68522dceb0933a9ed8e321b81bf0587b2568e8a5
snipersap <snipersap@gmail.com>
2023-09-11 21:36:23 +0200

    use sub struct

--- main.go
+++ main.go
@@ -6,24 +6,32 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
+}
+
+// Declare sub struct
+type contactInfo struct {
+	email   string
+	pincode int
 }
 
 func main() {
 	// Create person values
-	alex := person{"Alex", "Hardy"} // Order of the field is important
-	sandy := person{firstName: "Sandy", lastName: "Homie"}
+	alex := person{"Alex", "Hardy", contactInfo{"h@h.com", 99888}} // Order of the field is important, all fields must
+	sandy := person{firstName: "Sandy", lastName: "Homie", contact: contactInfo{"sandy.homie@gmail.com", 788999}}
 
 	// Print person values to the screen
-	alex.print()  //{Alex Hardy}
-	sandy.print() //{Sandy Homie}
+	alex.print()
+	sandy.print()
 
 	// Create person with default values
 	var priti person
-	priti.print()  //{ }
-	priti.printf() //{firstName: lastName:}
+	priti.print()
+	priti.printf()
 	priti.firstName = "Priti"
 	priti.lastName = "Sharma"
-	priti.printf() //{firstName:Priti lastName:Sharma}
+	priti.contact = contactInfo{email: "priti@sharma.com", pincode: 23099}
+	priti.printf()
 
 }
 
